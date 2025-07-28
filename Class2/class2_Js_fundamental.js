f704aeb76ad93224601c7c95e29a47b371c11a5d
Rohith2437 <manikandan.rohith@gmail.com>
2025-07-29 01:19:54 +0530

    Add files via upload
    class 2 assignments

---
+++ Class2/class2_Js_fundamental.js
@@ -0,0 +1,11 @@
+const browserVersion = 'chrome';
+ function getBrowserVersion(){
+
+    if(browserVersion ==='chrome'){
+        var localvariable =browserVersion;
+    }else{
+        console.log('The browser is not chrome' + browserVersion)
+    }
+ }
+console.log('the browser is '+browserVersion)
+getBrowserVersion();
---
+++ Class2/class2_primitve_datatypes.js
@@ -0,0 +1,11 @@
+let firstName ='Rohith'
+let companyName ='Conquer Technologies'
+let mobileNumber =9999999999
+let isAutomation =true;
+let hasPlaywright;
+
+console.log('the firstname is '+firstName)
+console.log('the companyName is '+companyName)
+console.log('the mobileNumber is '+mobileNumber)
+console.log('the isAutomation type is '+isAutomation)
+console.log('the hasPlaywright type is '+hasPlaywright)
